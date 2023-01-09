54b3449e0a5df039fb9d3ad740220659a7baaafa
m-inata <m.inata303@gmail.com>
2023-01-09 21:19:16 +0900

    add BlockBar, BlockL, BlockSq

---
+++ BlockGame/src/block/BlockBar.java
@@ -0,0 +1,170 @@
+package block;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import main.Field;
+import util.Point;
+
+/**
+ * 隣接する4つのboxから構成されるブロック
+ * [0][1][2][3]
+ */
+public class BlockBar extends Block {
+
+	public BlockBar(int x, int y, Color color) {
+		super(x, y, new ArrayList<Box>());
+		
+		boxes.add(new Box(this, -1, 0, color));
+		boxes.add(new Box(this, 0, 0, color));
+		boxes.add(new Box(this, 1, 0, color));
+		boxes.add(new Box(this, 2, 0, color));
+
+	}
+	
+	
+	/**
+	 * 他のブロックが部分的に消去後、1個のブロックが必要になったときのため。
+	 * @param x
+	 * @param y
+	 * @param color
+	 * @param points このブロックに含まれるboxの相対座標のリスト(要素が4個であると想定)
+	 */
+	public BlockBar(int x, int y, Color color, List<Point<Integer>> points) {
+		super(x, y, new ArrayList<Box>());
+		
+		for (Point<Integer> pt : points) {
+			boxes.add(new Box(this, pt.x(), pt.y(), color));
+		}
+	}
+
+	
+	/**
+	 * コピーコンストラクタ
+	 * @param other
+	 */
+	public BlockBar(BlockBar other) {
+		super(other);
+		
+		this.x = other.x;
+		this.y = other.y;
+	}
+
+	@Override
+	public boolean rotateRight(Field field) {
+		for (Box b : boxes) {
+			b.rotateRight();
+	    }
+	    if (!field.isIn(this) || !field.isVacant(this)) {
+	        this.x++;
+	    }
+	    if (!field.isIn(this) || !field.isVacant(this)) {
+	        this.x++;
+	    }
+	    if (!field.isIn(this) || !field.isVacant(this)) {
+	        this.x -= 3;
+	    }
+	    if (!field.isIn(this) || !field.isVacant(this)) {
+	        this.x--;
+	    }
+	    if (!field.isIn(this) || !field.isVacant(this)) {
+	        this.x += 2;
+	        for (Box b : boxes) {
+	        	b.rotateLeft();
+	        }
+	        return false;
+	    }
+	    return true;
+	}
+
+	@Override
+	public boolean rotateLeft(Field field) {
+		for (Box b : boxes) {
+			b.rotateLeft();
+	    }
+	    if (!field.isIn(this) || !field.isVacant(this)) {
+	        this.x++;
+	    }
+	    if (!field.isIn(this) || !field.isVacant(this)) {
+	        this.x++;
+	    }
+	    if (!field.isIn(this) || !field.isVacant(this)) {
+	        this.x -= 3;
+	    }
+	    if (!field.isIn(this) || !field.isVacant(this)) {
+	        this.x--;
+	    }
+	    if (!field.isIn(this) || !field.isVacant(this)) {
+	        this.x += 2;
+	        for (Box b : boxes) {
+	        	b.rotateRight();
+	        }
+	        return false;
+	    }
+	    return true;
+	}
+	
+	
+
+	/**
+	 * 指定された消去される位置をもとに、分解後のブロックのリストを構築する
+	 */
+	@Override
+	protected List<Block> getErased(Set<Integer> iBoxes) {
+		List<Block> ret = new ArrayList<>();
+				
+		if (iBoxes.containsAll(Set.of(0, 1, 2, 3))) {
+			;
+			
+		} else if (iBoxes.containsAll(Set.of(0, 1, 2))) {
+			ret = BlockCreater.create1(x, y, boxes, 3);
+			
+		} else if (iBoxes.containsAll(Set.of(0, 1, 3))) {
+			ret = BlockCreater.create1(x, y, boxes, 2);
+
+		} else if (iBoxes.containsAll(Set.of(0, 2, 3))) {
+			ret = BlockCreater.create1(x, y, boxes, 1);
+
+		} else if (iBoxes.containsAll(Set.of(1, 2, 3))) {
+			ret = BlockCreater.create1(x, y, boxes, 0);
+
+		} else if (iBoxes.containsAll(Set.of(0, 1))) {
+			ret = BlockCreater.create2(x, y, boxes, 2, 3);
+
+		} else if (iBoxes.containsAll(Set.of(0, 2))) {
+			ret = BlockCreater.create1_1(x, y, boxes, 1, 3);
+
+		} else if (iBoxes.containsAll(Set.of(0, 3))) {
+			ret = BlockCreater.create2(x, y, boxes, 1, 2);
+
+		} else if (iBoxes.containsAll(Set.of(1, 2))) {
+			ret = BlockCreater.create1_1(x, y, boxes, 0, 3);
+
+		} else if (iBoxes.containsAll(Set.of(1, 3))) {
+			ret = BlockCreater.create1_1(x, y, boxes, 0, 2);
+
+		} else if (iBoxes.containsAll(Set.of(2, 3))) {
+			ret = BlockCreater.create1_1(x, y, boxes, 0, 1);
+
+		} else if (iBoxes.containsAll(Set.of(0))) {
+			ret = BlockCreater.create3(x, y, boxes, 1, 2, 3);
+
+		} else if (iBoxes.containsAll(Set.of(1))) {
+			ret = BlockCreater.create1_2(x, y, boxes, 0, 2, 3);
+
+		} else if (iBoxes.containsAll(Set.of(2))) {
+			ret = BlockCreater.create1_2(x, y, boxes, 3, 1, 2);
+
+		} else if (iBoxes.containsAll(Set.of(3))) {
+			ret = BlockCreater.create3(x, y, boxes, 0, 1, 2);
+
+		} else {
+			ret.add(new BlockBar(this));
+		}
+		
+		return ret;
+	}
+
+}
---
+++ BlockGame/src/block/BlockCreater.java
@@ -0,0 +1,70 @@
+package block;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import util.Point;
+
+public class BlockCreater {
+	
+	public static List<Block> create1(int x, int y, List<Box> boxes, int i) {
+		List<Block> ret = new ArrayList<>();
+		Box b = boxes.get(1);
+		List<Point<Integer>> points = List.of(new Point<Integer>(b.getX(), b.getY()));
+		ret.add(new Block1(x, y, b.getColor(), points));
+		return ret;
+	}
+	
+	
+	public static List<Block> create2(int x, int y, List<Box> boxes, int i0, int i1) {
+		List<Block> ret = new ArrayList<>();
+		Box b = boxes.get(i0);
+		Box b2 = boxes.get(i1);
+		List<Point<Integer>> points = List.of(new Point<Integer>(b.getX(), b.getY()),
+				new Point<Integer>(b2.getX(), b2.getY()));
+		ret.add(new Block2(x, y, b.getColor(), points));
+		return ret;
+	}
+
+
+	public static List<Block> create1_1(int x, int y, List<Box> boxes, int i0, int i1) {
+		List<Block> ret = new ArrayList<>();
+		Box b = boxes.get(i0);
+		List<Point<Integer>> points = List.of(new Point<Integer>(b.getX(), b.getY()));
+		ret.add(new Block1(x, y, b.getColor(), points));
+		Box b2 = boxes.get(i1);
+		List<Point<Integer>> points2 = List.of(new Point<Integer>(b2.getX(), b2.getY()));
+		ret.add(new Block1(x, y, b.getColor(), points2));
+		return ret;
+	}
+
+
+	public static List<Block> create3(int x, int y, List<Box> boxes, int i0, int i1, int i2) {
+		List<Block> ret = new ArrayList<>();
+		Box b = boxes.get(i0);
+		Box b2 = boxes.get(i1);
+		Box b3 = boxes.get(i2);
+		List<Point<Integer>> points = List.of(new Point<Integer>(b.getX(), b.getY()),
+				new Point<Integer>(b2.getX(), b2.getY()),
+				new Point<Integer>(b3.getX(), b3.getY()));
+		ret.add(new BlockBar3(x, y, b.getColor(), points));
+		return ret;
+	}
+
+
+	public static List<Block> create1_2(int x, int y, List<Box> boxes, int i0, int i1, int i2) {
+		List<Block> ret = new ArrayList<>();
+		Box b = boxes.get(i0);
+		List<Point<Integer>> points = List.of(new Point<Integer>(b.getX(), b.getY()));
+		ret.add(new Block1(x, y, b.getColor(), points));
+		
+		Box b2 = boxes.get(i1);
+		Box b3 = boxes.get(i2);
+		List<Point<Integer>> points2 = List.of(new Point<Integer>(b2.getX(), b2.getY()),
+				new Point<Integer>(b3.getX(), b3.getY()));
+		ret.add(new Block2(x, y, b.getColor(), points2));
+		return ret;
+	}
+
+
+}
